83da039d3bc055792c7117247c65f50dc47e16ea
Isaac Cohen <icohen@leapmotion.com>
2014-05-11 18:35:53 -0700

    adding gitignore , stabilizing

--- wombs-audio-component.js
+++ wombs-audio-component.js
@@ -3,43 +3,45 @@
   var Texture = require( 'wombs-audio-texture' );
   var _ = require( 'underscore' );
  
-
-  var mutate = {};
+  
+  var mutation = {};
 
 
   // mutes output
-  mutate.mute = function(){
+  mutation.mute = function(){
     this.output.gain.value = 0;
   }
 
-  mutate.unmute = function( value ){
+  mutation.unmute = function( value ){
     value = value || 1;
     this.output.gain.value = value;
   }
 
   // silences input
-  mutate.silence = function(){
+  mutation.silence = function(){
     this.input.gain.value = 0;
   }
   
-  mutate.unsilence = function(){
+  mutation.unsilence = function(){
     value = value || 1;
     this.input.gain.value = value;
   }
 
   // Moves things out of this 
-  mutate.import = function( node ){
+  mutation.import = function( node ){
     node.connect( this.input );
   }
 
-  mutate.export = function( node ){
+  mutation.export = function( node ){
     this.output.connect( node );
   }
 
 
 
   //creates an analyser with which ever input desired 
-  mutate.createAnalyser = function( fbc , i ){
+  mutation.createAnalyser = function( fbc , i ){
+
+    console.log( this );
 
     fbc = fbc ||  1024 * 2;
     i = i || this.input;
@@ -51,7 +53,7 @@
 
   }
 
-  mutate.insertNode = function( node , i , o ){
+  mutation.insertNode = function( node , i , o ){
 
     console.log( node );
     console.log( i );
@@ -62,7 +64,7 @@
 
   }
   
-  mutate.setFrequencyBinCount = function( fbc , i ){
+  mutation.setFrequencyBinCount = function( fbc , i ){
  
     this.createAnalyser( fbc , i );
 
@@ -77,7 +79,7 @@
   // and title it anything, but by default, 
   // there should be one this.texture, if you are
   // trying to do it simply
-  mutate.createTexture = function( analyser , title  ){
+  mutation.createTexture = function( analyser , title  ){
 
     title = title || 'texture'
     this[title] = new Texture( analyser );
@@ -85,11 +87,10 @@
 
   }
   
-
-  AudioComponent.prototype = _.extend( 
-      Component.prototype, 
-      mutate 
-  );
+  AudioComponent.prototype = _.extend(
+    Component.prototype,
+    mutation
+  ); 
 
   function AudioComponent( parentNode ){
 
